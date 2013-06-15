596a045c1316627915f54d648671f82da9eaa9f9
Brett Jackson <brett@brettjackson.org>
2013-06-15 14:02:53 -0500

    added factorial, notebook

--- tests.rs
+++ tests.rs
@@ -2,5 +2,7 @@ extern mod math;
 
 fn main() {
     let result = math::sin(1.9);
-    println(fmt!("%?",result));
+    println(fmt!("%?", result));
+    let intresult = math::factorial(-1);
+    println(fmt!("%?", intresult));
 }
